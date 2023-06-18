921f650e6f8e01835b3165c788a5196742ece049
Sven Maack <sven.maack@gmail.com>
2023-06-18 14:05:24 +0200

    add git status to git workflow

--- src/tcr.sh
+++ src/tcr.sh
@@ -1,1 +1,1 @@
-gradle check && git commit -a || (git reset --hard && google-chrome "http://localhost:63342/kte/src/build/reports/tests/nativeTest/index.html?&_ij_reload=RELOAD_ON_SAVE")
+git status && gradle check && git commit -a || (git reset --hard && google-chrome "http://localhost:63342/kte/src/build/reports/tests/nativeTest/index.html?&_ij_reload=RELOAD_ON_SAVE")
