0238275b79d3eeffe2b5b36a2a3279c758989ac4
Sven Maack <sven.maack@pmagentur.com>
2023-09-02 14:59:20 +0200

    allow all body tags in body container tags

---
+++ src/src/nativeMain/kotlin/lib/BodyTagContainer.kt
@@ -0,0 +1,14 @@
+package lib
+
+import lib.base.TagContainer
+
+open class BodyTagContainer(name: String) : TagContainer(name) {
+    inline fun p(init: P.() -> Unit) = add(P(), init)
+    inline fun h1(init: H1.() -> Unit) = add(H1(), init)
+    inline fun h2(init: H2.() -> Unit) = add(H2(), init)
+    inline fun h3(init: H3.() -> Unit) = add(H3(), init)
+    inline fun h4(init: H4.() -> Unit) = add(H4(), init)
+    inline fun h5(init: H5.() -> Unit) = add(H5(), init)
+    inline fun h6(init: H6.() -> Unit) = add(H6(), init)
+    inline fun br() = add(Br()) {}
+}
