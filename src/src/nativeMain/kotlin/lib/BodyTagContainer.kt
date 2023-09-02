f3c04221bbb3932e53ff7f3862c5bdaa85f23e3d
Sven Maack <sven.maack@pmagentur.com>
2023-09-02 15:00:48 +0200

    adding div

--- src/src/nativeMain/kotlin/lib/BodyTagContainer.kt
+++ src/src/nativeMain/kotlin/lib/BodyTagContainer.kt
@@ -10,5 +10,6 @@ open class BodyTagContainer(name: String) : TagContainer(name) {
     inline fun h4(init: H4.() -> Unit) = add(H4(), init)
     inline fun h5(init: H5.() -> Unit) = add(H5(), init)
     inline fun h6(init: H6.() -> Unit) = add(H6(), init)
+    inline fun div(init: Div.() -> Unit) = add(Div(), init)
     inline fun br() = add(Br()) {}
 }
