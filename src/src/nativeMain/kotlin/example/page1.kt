38c1500ce1315f4e926819af07ebc52b1bbeec3c
Sven Maack <sven.maack@pmagentur.com>
2023-09-02 15:01:00 +0200

    removing goal from example

--- src/src/nativeMain/kotlin/example/page1.kt
+++ src/src/nativeMain/kotlin/example/page1.kt
@@ -14,30 +14,11 @@ val page1: Element =
             h1 {
                 +"Hello, world!"
             }
-            p {
-                +"This is my first web page."
+            div {
+                p {
+                    +"This is my first web page."
+                }
             }
             br()
         }
     }
-
-
-/**
- * <!DOCTYPE html>
- * <html>
- *     <head>
- *         <title>
- *             My web page
- *         </title>
- *     </head>
- *     <body>
- *         <h1>
- *             Hello, world!
- *         </h1>
- *         <p class="myClass">
- *             This is my first web page.
- *         </p>
- *         </br>
- *     </body>
- * </html>
- */
