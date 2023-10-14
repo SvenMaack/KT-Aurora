3d2b63969005eb9d0d10868bb3cf1604c5471419
Sven Maack <sven.maack@pmagentur.com>
2023-10-14 22:07:47 +0200

    save html in file

--- src/src/nativeMain/kotlin/Main.kt
+++ src/src/nativeMain/kotlin/Main.kt
@@ -1,9 +1,27 @@
 import example.page1
+import kotlinx.cinterop.memScoped
 import lib.DebugVisitor
+import platform.posix.EOF
+import platform.posix.fclose
+import platform.posix.fopen
+import platform.posix.fputs
+
+fun writeAllText(filePath:String, text:String) {
+    val file = fopen(filePath, "w") ?:
+        throw IllegalArgumentException("Cannot open output file $filePath")
+    try {
+        memScoped {
+            if(fputs(text, file) == EOF) throw Error("File write error")
+        }
+    } finally {
+        fclose(file)
+    }
+}
 
 fun main() {
     with(DebugVisitor()) {
         page1.traverse(this)
         println(this.html)
+        writeAllText("./test.html", this.html)
     }
 }
