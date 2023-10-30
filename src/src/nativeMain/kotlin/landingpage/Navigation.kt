5ff864899695722d4c1c3403447788c94df3b97b
Sven Maack <sven.maack@pmagentur.com>
2023-10-30 16:47:38 +0100

    add navigation css

--- src/src/nativeMain/kotlin/landingpage/Navigation.kt
+++ src/src/nativeMain/kotlin/landingpage/Navigation.kt
@@ -1,5 +1,8 @@
 package landingpage
 
+import css_lib.base.Document
+import css_lib.properties.margin
+import css_lib.properties.px
 import template_lib.StaticTemplate
 import template_lib.childs
 import template_lib.tags.Div
@@ -8,16 +11,25 @@ val Navigation: StaticTemplate = { _ ->
     Div().childs {
         nav(clazz="navigation") {
             ul {
-                li {
+                li(clazz="navigation__item") {
                     span{+"Home"}
                 }
-                li {
+                li(clazz="navigation__item") {
                     span{+"Trends"}
                 }
-                li {
+                li(clazz="navigation__item") {
                     span{+"Sales"}
                 }
             }
         }
+    }
+}
+
+val navigationCssDocument: Document = Document().apply {
+    this[".navigation"] = {
+        margin(50.px)
+    }
+    this[".navigation__item"] = {
+        margin(15.px)
     }
 }
