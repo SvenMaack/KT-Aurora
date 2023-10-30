59782aad2407601e32e3de65bdd76e87d9ce7570
Sven Maack <sven.maack@pmagentur.com>
2023-10-30 12:17:31 +0100

    add css rule

---
+++ src/src/nativeMain/kotlin/css_lib/Rule.kt
@@ -0,0 +1,14 @@
+package css_lib
+
+data class Rule(
+    val selector: String,
+    val properties: MutableList<Property> = mutableListOf()
+) {
+    fun add(property: Property): Rule {
+        properties.add(property)
+        return this
+    }
+}
+
+inline infix fun String.rule(init: Rule.() -> Unit): Rule =
+    Rule(this).apply(init)
