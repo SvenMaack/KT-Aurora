b39fa411072ed524525751edd145effd46315deb
Sven Maack <sven.maack@pmagentur.com>
2023-11-01 12:57:59 +0100

    add position tests

--- src/src/nativeMain/kotlin/css_lib/properties/Position.kt
+++ src/src/nativeMain/kotlin/css_lib/properties/Position.kt
@@ -1,8 +1,9 @@
+@file:Suppress("unused")
+
 package css_lib.properties
 
 import css_lib.base.Property
 import css_lib.base.Rule
-import css_lib.base.browser.Support
 import css_lib.base.browser.SupportImpl
 
 //https://www.w3schools.com/cssref/pr_class_position.php
@@ -33,57 +34,79 @@ enum class PositionDistanceValue(val value: String) {
 }
 
 fun Rule.position(position: PositionValue): Property =
-    +object : Property("position", position.value), Support by SupportImpl(
-        chrome = { 1.0 },
-        edge = { 7.0 },
-        firefox = { 1.0 },
-        safari = { 1.0 },
-        opera = { 4.0 },
-    ) {
-        override val defaultValue: String
-            get() = PositionValue.static.value
-    }
+    +Property.build(
+        property = "position",
+        value = position.value,
+        defaultValue = PositionValue.static.value,
+        supportedBrowsers = SupportImpl(
+            chrome = { 1.0 },
+            edge = { 7.0 },
+            firefox = { 1.0 },
+            safari = { 1.0 },
+            opera = { 4.0 },
+        ).supportedBrowsers
+    )
 
 fun Rule.top(size: Size): Property =
-    +object : Property("top", size.toString()), Support by support {
-        override val defaultValue: String
-            get() = PositionValue.static.value
-    }
+    +Property.build(
+        property = "top",
+        value = size.toString(),
+        defaultValue = PositionValue.static.value,
+        supportedBrowsers = support.supportedBrowsers
+    )
+
 fun Rule.top(value: PositionDistanceValue): Property =
-    +object : Property("top", value.value), Support by support {
-        override val defaultValue: String
-            get() = PositionValue.static.value
-    }
+    +Property.build(
+        property = "top",
+        value = value.value,
+        defaultValue = PositionValue.static.value,
+        supportedBrowsers = support.supportedBrowsers
+    )
 
 fun Rule.right(size: Size): Property =
-    +object : Property("right", size.toString()), Support by support {
-        override val defaultValue: String
-            get() = PositionValue.static.value
-    }
+    +Property.build(
+        property = "right",
+        value = size.toString(),
+        defaultValue = PositionValue.static.value,
+        supportedBrowsers = support.supportedBrowsers
+    )
+
 fun Rule.right(value: PositionDistanceValue): Property =
-    +object : Property("right", value.value), Support by support {
-        override val defaultValue: String
-            get() = PositionValue.static.value
-    }
+    +Property.build(
+        property = "right",
+        value = value.value,
+        defaultValue = PositionValue.static.value,
+        supportedBrowsers = support.supportedBrowsers
+    )
 
 fun Rule.bottom(size: Size): Property =
-    +object : Property("bottom", size.toString()), Support by support {
-        override val defaultValue: String
-            get() = PositionValue.static.value
-    }
+    +Property.build(
+        property = "bottom",
+        value = size.toString(),
+        defaultValue = PositionValue.static.value,
+        supportedBrowsers = support.supportedBrowsers
+    )
+
 fun Rule.bottom(value: PositionDistanceValue): Property =
-    +object : Property("bottom", value.value), Support by support {
-        override val defaultValue: String
-            get() = PositionValue.static.value
-    }
+    +Property.build(
+        property = "bottom",
+        value = value.value,
+        defaultValue = PositionValue.static.value,
+        supportedBrowsers = support.supportedBrowsers
+    )
 
 fun Rule.left(size: Size): Property =
-    +object : Property("left", size.toString()), Support by support {
-        override val defaultValue: String
-            get() = PositionValue.static.value
-    }
+    +Property.build(
+        property = "left",
+        value = size.toString(),
+        defaultValue = PositionValue.static.value,
+        supportedBrowsers = support.supportedBrowsers
+    )
+
 fun Rule.left(value: PositionDistanceValue): Property =
-    +object : Property("left", value.value), Support by support {
-        override val defaultValue: String
-            get() = PositionValue.static.value
-    }
+    +Property.build(
+        property = "left",
+        value = value.value,
+        defaultValue = PositionValue.static.value,
+        supportedBrowsers = support.supportedBrowsers
+    )
--- src/src/nativeTest/kotlin/css_lib/properties/PositionTest.kt
+++ src/src/nativeTest/kotlin/css_lib/properties/PositionTest.kt
@@ -6,92 +6,73 @@ import kotlin.test.assertEquals
 
 class PositionTest {
     @Test
-    fun `test position static`() {
-        val rule = Rule("selector")
-        rule.position(PositionValue.static)
-
-        assertEquals(1, rule.properties.size)
-        assertEquals("position", rule.properties[0].property)
-        assertEquals("static", rule.properties[0].value)
-    }
-
-    @Test
-    fun `test position relative`() {
-        val rule = Rule("selector")
-        rule.position(PositionValue.relative)
-
-        assertEquals(1, rule.properties.size)
-        assertEquals("position", rule.properties[0].property)
-        assertEquals("relative", rule.properties[0].value)
-    }
-
-    @Test
-    fun `test position fixed`() {
-        val rule = Rule("selector")
-        rule.position(PositionValue.fixed)
-
-        assertEquals(1, rule.properties.size)
-        assertEquals("position", rule.properties[0].property)
-        assertEquals("fixed", rule.properties[0].value)
-    }
-
-    @Test
-    fun `test position absolute`() {
-        val rule = Rule("selector")
-        rule.position(PositionValue.absolute)
-
-        assertEquals(1, rule.properties.size)
-        assertEquals("position", rule.properties[0].property)
-        assertEquals("absolute", rule.properties[0].value)
-    }
-
-    @Test
-    fun `test position sticky`() {
-        val rule = Rule("selector")
-        rule.position(PositionValue.sticky)
-
-        assertEquals(1, rule.properties.size)
-        assertEquals("position", rule.properties[0].property)
-        assertEquals("sticky", rule.properties[0].value)
+    fun `test position`() {
+        PositionValue.entries.forEach {
+            val rule = Rule("selector")
+            rule.position(it)
+            check(rule, "position", it.value)
+        }
     }
 
     @Test
     fun `test top`() {
         val rule = Rule("selector")
-        rule.top(5.px)
+        rule.top(PositionDistanceValue.inherit)
+        check(rule, "top", "inherit")
+    }
 
-        assertEquals(1, rule.properties.size)
-        assertEquals("top", rule.properties[0].property)
-        assertEquals("5px", rule.properties[0].value)
+    @Test
+    fun `test top-value`() {
+        val rule = Rule("selector")
+        rule.top(5.px)
+        check(rule, "top", "5px")
     }
 
     @Test
     fun `test right`() {
         val rule = Rule("selector")
-        rule.right(5.px)
+        rule.right(PositionDistanceValue.inherit)
+        check(rule, "right", "inherit")
+    }
 
-        assertEquals(1, rule.properties.size)
-        assertEquals("right", rule.properties[0].property)
-        assertEquals("5px", rule.properties[0].value)
+    @Test
+    fun `test right-value`() {
+        val rule = Rule("selector")
+        rule.right(5.px)
+        check(rule, "right", "5px")
     }
 
     @Test
     fun `test bottom`() {
         val rule = Rule("selector")
-        rule.bottom(5.px)
+        rule.bottom(PositionDistanceValue.inherit)
+        check(rule, "bottom", "inherit")
+    }
 
-        assertEquals(1, rule.properties.size)
-        assertEquals("bottom", rule.properties[0].property)
-        assertEquals("5px", rule.properties[0].value)
+    @Test
+    fun `test bottom-value`() {
+        val rule = Rule("selector")
+        rule.bottom(5.px)
+        check(rule, "bottom", "5px")
     }
 
     @Test
     fun `test left`() {
         val rule = Rule("selector")
-        rule.left(5.px)
+        rule.left(PositionDistanceValue.inherit)
+        check(rule, "left", "inherit")
+    }
 
+    @Test
+    fun `test left-value`() {
+        val rule = Rule("selector")
+        rule.left(5.px)
+        check(rule, "left", "5px")
+    }
+
+    private fun check(rule: Rule, property: String, value: String) {
         assertEquals(1, rule.properties.size)
-        assertEquals("left", rule.properties[0].property)
-        assertEquals("5px", rule.properties[0].value)
+        assertEquals(property, rule.properties[0].property)
+        assertEquals(value, rule.properties[0].value)
     }
 }
