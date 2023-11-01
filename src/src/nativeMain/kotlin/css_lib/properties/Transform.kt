81b0bc8f5a48c348515a279f54f899a634cdb769
Sven Maack <sven.maack@pmagentur.com>
2023-11-01 13:06:14 +0100

    improve transform

--- src/src/nativeMain/kotlin/css_lib/properties/Transform.kt
+++ src/src/nativeMain/kotlin/css_lib/properties/Transform.kt
@@ -2,28 +2,32 @@ package css_lib.properties
 
 import css_lib.base.Property
 import css_lib.base.Rule
-import css_lib.base.browser.Support
 import css_lib.base.browser.SupportImpl
 
-fun Rule.transform(x: Size, y: Size): Property {
-    +object : Property("-webkit-transform", "translate($x,$y)"), Support by SupportImpl(
-        chrome = { 36.0 },
-        edge = { 10.0 },
-        firefox = { 16.0 },
-        safari = { 9.0 },
-        opera = { 23.0 },
-    ) {
-        override val defaultValue: String?
-            get() = null
-    }
-    return +object : Property("transform", "translate($x,$y)"), Support by SupportImpl(
-        chrome = { 36.0 },
-        edge = { 10.0 },
-        firefox = { 16.0 },
-        safari = { 9.0 },
-        opera = { 23.0 },
-    ) {
-        override val defaultValue: String?
-            get() = null
-    }
-}
+fun Rule.transform(x: Size, y: Size): List<Property> =
+    listOf(
+        +Property.build(
+            property = "-webkit-transform",
+            value = "translate($x,$y)",
+            defaultValue = null,
+            supportedBrowsers = SupportImpl(
+                chrome = { 36.0 },
+                edge = { 10.0 },
+                firefox = { 16.0 },
+                safari = { 9.0 },
+                opera = { 23.0 },
+            ).supportedBrowsers
+        ),
+        +Property.build(
+            property = "transform",
+            value = "translate($x,$y)",
+            defaultValue = null,
+            supportedBrowsers = SupportImpl(
+                chrome = { 36.0 },
+                edge = { 10.0 },
+                firefox = { 16.0 },
+                safari = { 9.0 },
+                opera = { 23.0 },
+            ).supportedBrowsers
+        )
+    )
