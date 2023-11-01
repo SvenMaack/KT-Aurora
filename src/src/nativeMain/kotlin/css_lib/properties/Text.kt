98b4ade08607fb2dc1e30f6ee22c86c12679407a
Sven Maack <sven.maack@pmagentur.com>
2023-11-01 13:01:00 +0100

    add text tests

--- src/src/nativeMain/kotlin/css_lib/properties/Text.kt
+++ src/src/nativeMain/kotlin/css_lib/properties/Text.kt
@@ -1,16 +1,10 @@
-@file:Suppress("FunctionName")
+@file:Suppress("FunctionName", "unused")
 
 package css_lib.properties
 
 import css_lib.base.Property
 import css_lib.base.Rule
-import css_lib.base.browser.Support
 import css_lib.base.browser.SupportImpl
-
-@Suppress("EnumEntryName")
-enum class TextDecorationValues(val value: String) {
-    none("none"),
-}
 
 @Suppress("EnumEntryName")
 enum class TextTransformValues(val value: String) {
@@ -23,13 +17,15 @@ enum class TextTransformValues(val value: String) {
 }
 
 fun Rule.`text-transform`(style: TextTransformValues): Property =
-    +object : Property("text-transform", style.value), Support by SupportImpl(
-        chrome = { 1.0 },
-        edge = { 4.0 },
-        firefox = { 1.0 },
-        safari = { 1.0 },
-        opera = { 7.0 },
-    ) {
-        override val defaultValue: String?
-            get() = null
-    }
+    +Property.build(
+        property = "text-transform",
+        value = style.value,
+        defaultValue = null,
+        supportedBrowsers = SupportImpl(
+            chrome = { 1.0 },
+            edge = { 4.0 },
+            firefox = { 1.0 },
+            safari = { 1.0 },
+            opera = { 7.0 },
+        ).supportedBrowsers
+    )
--- src/src/nativeTest/kotlin/css_lib/properties/TextTest.kt
+++ src/src/nativeTest/kotlin/css_lib/properties/TextTest.kt
@@ -7,11 +7,13 @@ import kotlin.test.assertEquals
 class TextTest {
     @Test
     fun `test text-transform`() {
-        val rule = Rule("selector")
-        rule.`text-transform`(TextTransformValues.uppercase)
+        TextTransformValues.entries.forEach {
+            val rule = Rule("selector")
+            rule.`text-transform`(it)
 
-        assertEquals(1, rule.properties.size)
-        assertEquals("text-transform", rule.properties[0].property)
-        assertEquals("uppercase", rule.properties[0].value)
+            assertEquals(1, rule.properties.size)
+            assertEquals("text-transform", rule.properties[0].property)
+            assertEquals(it.value, rule.properties[0].value)
+        }
     }
 }
