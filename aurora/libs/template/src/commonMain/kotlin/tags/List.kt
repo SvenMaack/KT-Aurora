2b9cfbcf11815703742d8773f98f620386450203
Sven Maack <sven.maack@pmagentur.com>
2023-11-26 15:25:21 +0100

    adding menu tag

--- aurora/libs/template/src/commonMain/kotlin/tags/List.kt
+++ aurora/libs/template/src/commonMain/kotlin/tags/List.kt
@@ -17,4 +17,11 @@ public class Ol: BodyTagContainer("ol") {
     }
 }
 
+public class Menu: BodyTagContainer("menu") {
+    public inline fun li(vararg attributes: Attribute, clazz: String? = null, init: Li.() -> Unit): Li = add(Li()) {
+        if(clazz==null) setAttributes(*attributes) else setAttributes(*attributes, "class"[clazz])
+        init()
+    }
+}
+
 public class Li: BodyTagContainer("li")
