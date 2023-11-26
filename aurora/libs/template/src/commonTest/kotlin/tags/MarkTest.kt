5274fd964b259810930be1085e13801f25599093
Sven Maack <sven.maack@pmagentur.com>
2023-11-26 15:21:38 +0100

    add mark tag

---
+++ aurora/libs/template/src/commonTest/kotlin/tags/MarkTest.kt
@@ -0,0 +1,12 @@
+package template_lib.tags
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MarkTest {
+    @Test
+    fun `tag name is mark`() {
+        val tag = Mark()
+        assertEquals("mark", tag.name)
+    }
+}
