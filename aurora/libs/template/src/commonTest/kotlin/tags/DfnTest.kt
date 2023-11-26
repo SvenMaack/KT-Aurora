1785aab5d32213be0b18c405dcdc0dba58d05873
Sven Maack <sven.maack@pmagentur.com>
2023-11-26 14:58:11 +0100

    add dfn tag

---
+++ aurora/libs/template/src/commonTest/kotlin/tags/DfnTest.kt
@@ -0,0 +1,12 @@
+package template_lib.tags
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DfnTest {
+    @Test
+    fun `tag name is dfn`() {
+        val tag = Dfn()
+        assertEquals("dfn", tag.name)
+    }
+}
