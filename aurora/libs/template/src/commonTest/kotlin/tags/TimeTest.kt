679ca8761a64b7b2962cbdbeba69681eccaec56b
Sven Maack <sven.maack@pmagentur.com>
2023-11-26 15:51:24 +0100

    add time tag

---
+++ aurora/libs/template/src/commonTest/kotlin/tags/TimeTest.kt
@@ -0,0 +1,12 @@
+package template_lib.tags
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TimeTest {
+    @Test
+    fun `tag name is time`() {
+        val tag = Time()
+        assertEquals("time", tag.name)
+    }
+}
