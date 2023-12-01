34efc5854a8aaa9b179f7b07e27e1f0394726007
Sven Maack <sven.maack@pmagentur.com>
2023-12-01 21:16:55 +0100

    add comments to html tags

--- aurora/libs/template/src/commonMain/kotlin/tags/DescriptionList.kt
+++ aurora/libs/template/src/commonMain/kotlin/tags/DescriptionList.kt
@@ -9,11 +9,17 @@ public class Dt: TagContainer("dt")
 public class Dd: TagContainer("dd")
 
 public class Dl: TagContainer("dl") {
+    /**
+     * The tag defines a term/name in a description list.
+     */
     public inline fun dt(vararg attributes: Attribute, clazz: String? = null, init: Dt.() -> Unit): Dt = add(Dt()) {
         if(clazz==null) setAttributes(*attributes) else setAttributes(*attributes, "class"[clazz])
         init()
     }
 
+    /**
+     * The tag is used to describe a term/name in a description list.
+     */
     public inline fun dd(vararg attributes: Attribute, clazz: String? = null, init: Dd.() -> Unit): Dd = add(Dd()) {
         if(clazz==null) setAttributes(*attributes) else setAttributes(*attributes, "class"[clazz])
         init()
