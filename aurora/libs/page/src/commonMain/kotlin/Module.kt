260c612e3e08fec51c96a30238de5acff26d9e2d
Sven Maack <sven.maack@pmagentur.com>
2023-12-07 12:26:29 +0100

    make fields in module optional

--- aurora/libs/page/src/commonMain/kotlin/Module.kt
+++ aurora/libs/page/src/commonMain/kotlin/Module.kt
@@ -1,15 +1,17 @@
 package page_lib
 
+import css.base.EmptyDocument
 import css.base.IDocument
 import template.Template
 import template.TemplateExtension
+import template.base.TransientTag
 
 data class Module<TAG, VM> (
-    val document: IDocument,
-    val template: TemplateExtension<TAG, VM>
+    val document: IDocument = EmptyDocument,
+    val template: TemplateExtension<TAG, VM> = { _, _ -> }
 )
 
 data class PageModule<VM> (
-    val document: IDocument,
-    val template: Template<VM>
+    val document: IDocument = EmptyDocument,
+    val template: Template<VM> = { _, _ -> TransientTag()}
 )
