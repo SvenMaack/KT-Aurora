ba8c534fdfffd28a78a2fc704d7fe221000182b5
Sven Maack <sven.maack@pmagentur.com>
2023-12-16 15:50:19 +0100

    allow unregistered query parameters in the navigation

--- aurora/apps/landingpage/src/nativeMain/kotlin/landingPage/domain/NavigationProvider.kt
+++ aurora/apps/landingpage/src/nativeMain/kotlin/landingPage/domain/NavigationProvider.kt
@@ -14,13 +14,15 @@ internal class NavigationProvider: INavigationProvider {
     override suspend fun getNavigation(currentPath: String): Navigation {
         return DummyNavigation(
             listOf(
-                DummyNavigationEntry("Home", url = "/?id=1", active = "/?id=1" == currentPath),
-                DummyNavigationEntry("Men", url = "/?id=2", active = "/?id=2" == currentPath),
-                DummyNavigationEntry("Women", url = "/?id=3", active = "/?id=3" == currentPath),
-                DummyNavigationEntry("Kids", url = "/?id=4", active = "/?id=4" == currentPath),
-                DummyNavigationEntry("Collection", url = "/?id=5", active = "/?id=5" == currentPath),
-                DummyNavigationEntry("Trends", url = "/?id=6", active = "/?id=6" == currentPath),
-            )
+                DummyNavigationEntry("Home", url = "/?id=1"),
+                DummyNavigationEntry("Men", url = "/?id=2"),
+                DummyNavigationEntry("Women", url = "/?id=3"),
+                DummyNavigationEntry("Kids", url = "/?id=4"),
+                DummyNavigationEntry("Collection", url = "/?id=5"),
+                DummyNavigationEntry("Trends", url = "/?id=6"),
+            ).map {
+                it.copy(active = currentPath.startsWith(it.url))
+            }
         )
     }
 }
